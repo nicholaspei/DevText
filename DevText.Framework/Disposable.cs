a0acf2752baca5bfbe7e35c1a23048d49cdae478
nicholaspei <nic162534@gmail.com>
2010-12-04 20:29:44 +0800

    Sourcecode Checkin #DevText :)

---
+++ DevText.Framework/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DevText.Framework
+{
+    public abstract class Disposable:IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void DisposeCore()
+        {
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+    }
+}
---
+++ DevText.Framework/Extensions/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace DevText.Framework.Extensions
+{
+
+    public static class ReaderWriterLockSlimExtensions
+    {
+        public static IDisposable ReadAndMaybeWrite(this ReaderWriterLockSlim instance)
+        {
+            
+            instance.EnterUpgradeableReadLock();
+
+            return new SynchronizedCodeBlock(instance.ExitUpgradeableReadLock);
+        }
+
+        public static IDisposable Read(this ReaderWriterLockSlim instance)
+        {
+            instance.EnterReadLock();
+
+            return new SynchronizedCodeBlock(instance.ExitReadLock);
+        }
+
+        public static IDisposable Write(this ReaderWriterLockSlim instance)
+        {
+            
+            instance.EnterWriteLock();
+
+            return new SynchronizedCodeBlock(instance.ExitWriteLock);
+        }
+
+        private sealed class SynchronizedCodeBlock : IDisposable
+        {
+            private readonly Action action;
+
+            public SynchronizedCodeBlock(Action action)
+            {
+                this.action = action;
+            }
+
+            public void Dispose()
+            {
+                action();
+            }
+        }
+    }
+}
