61fdc2bdcaac056019ac010590947e4c44ba4b12
nicholaspei <nic162534@gmail.com>
2010-12-04 11:51:00 +0800

    #DevText framework Service sourcecode checkin

---
+++ DevText.Framework/Services/RuleViolation.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace DevText.Framework.Services
+{
+    public class RuleViolation
+    {
+
+        public RuleViolation(string parameterName,string errorMessage)
+        {
+            ParameterName = parameterName;
+            ErrorMessage = errorMessage;
+        }
+
+        public string ParameterName
+        {
+            get;
+            private set;
+        }
+
+        public string ErrorMessage
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ DevText.Framework/Services/ServiceResultBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DevText.Framework.Services
+{
+    public abstract class ServiceResultBase
+    {
+        protected ServiceResultBase(IEnumerable<RuleViolation> ruleViolations)
+        {
+            RuleViolations = new List<RuleViolation>(ruleViolations);
+        }
+        
+        public IList<RuleViolation> RuleViolations
+        {
+            get;
+            private set;
+        }
+ 
+    }
+}
